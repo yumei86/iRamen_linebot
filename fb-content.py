f852321528749af3d4ca25e9ee140def10aa8bde
troublecat55 <yuchaodemmi@gmail.com>
2020-12-01 00:08:07 +0100

    organize store and ramen name

--- fb-content.py
+++ fb-content.py
@@ -2,11 +2,10 @@ import pandas as pd
 f = open('1_fb_crawling.txt', 'r', encoding='utf8')
 shop_count = 0
 ramen_list = []
+ramen_shop_raw = []
 ramen_shop_list = []
 ramen_name_list = []
 ramen_name_raw = []
-ramen_name_raw_zero = []
-ramen_name_M = []
 ramen_review_raw = []
 ramen_review_list = []
 unorganized_shops = []
@@ -19,6 +18,7 @@ for line in f:
     #### catch 店家:(first item of each sublist) to % for 店名
     #### catch G to Z, if not Z catch to the first 0
     start_point = line.replace('… 更多','').replace(' ','').replace('$','').replace('%','')\
+                    .replace(u'\u3000',u'')\
                     .replace('／','').replace('區域','').replace('：',':').replace(':',':')\
                     .replace('店家:','$店家:')\
                     .replace('▎店家:','$店家:').replace('▎店　　名:','$店家:')\
@@ -53,10 +53,10 @@ for shops in ramen_list:
         or shops.index('Z')>shops.index('G')+80) :
         unorganized_shops.append(shops)
     else:
-        ramen_shop_list.append(shops[:shops.index('%')])
+        ramen_shop_raw.append(shops[:shops.index('%')])
         ramen_name_raw.append(shops[shops.index('G')+1:shops.index('Z')])
         ramen_review_raw.append(shops[shops.index('Z')+1:shops.index('Z')+265]+'...')
-# print(ramen_shop_list)
+# print(len(ramen_shop_list))
 # print(ramen_name_list)
 # print(ramen_review_list)
 # print(ramen_list)
@@ -64,70 +64,84 @@ for shops in ramen_list:
 #### second filtering
 for shops in unorganized_shops:
     if ('G' in shops and '0' in shops):
-        ramen_shop_list.append(shops[:shops.index('%')])
-        ramen_name_raw.append(shops[shops.index('G')+1:shops.index('G')+25])
+        ramen_shop_raw.append(shops[:shops.index('%')])
+        ramen_name_raw.append(shops[shops.index('G')+1:shops.index('G')+30])
         ramen_review_raw.append(shops[shops.index('G')+19:shops.index('G')+285]+'...')
 
     else:
         unorganized_unorganized_shops.append(shops)
 
+# print(ramen_shop_raw)
+
+for shops in ramen_shop_raw:
+    new_shops = shops.replace('■','').replace('2.','')
+    if '地址' in new_shops and '用餐' in new_shops:
+        # print(shops)
+        if new_shops.index('地') < new_shops.index('用'):
+            ramen_shop_list.append(new_shops[:new_shops.index('地')])
+        else:
+            ramen_shop_list.append(new_shops[:new_shops.index('用')])
+    elif '地址' in new_shops:
+        ramen_shop_list.append(new_shops[:new_shops.index('地')])
+    elif '用餐' in new_shops:
+        ramen_shop_list.append(new_shops[:new_shops.index('用')])
+    else:
+        ramen_shop_list.append(new_shops)
+
+
+# print(ramen_shop_list)
+for names in ramen_name_raw:
+    # print(f'original name{names}')
+    new_name = names.replace('拉麵%.G','').replace('%.G','')
+    last_ch = new_name[-1]
+    first_ch = new_name[0]
+    # ramen_name_list = []
+    if '0' in new_name and '00' not in new_name and '2020' not in new_name\
+        and last_ch != '）' and last_ch != ')':
+        ramen_name_list.append(new_name[:new_name.index('0')+1])
+    elif '00' in new_name and last_ch != '）' and last_ch != ')'\
+        and '2020' not in new_name:
+        ramen_name_list.append(new_name[:new_name.index('00')+2])
+    elif first_ch != '拉':
+        ramen_name_list.append(new_name[new_name.index('拉'):])
+    elif '0' not in new_name and '00' not in new_name and '/' not in new_name\
+        and last_ch.isdigit() == False and last_ch != '麵' and last_ch != '麺'\
+        and last_ch != '）' and last_ch != ')'and '+' not in new_name:
+        new_point =  new_name.replace('麵','麵H').replace('麺','麵H').replace('拉麵H名稱','拉麵名稱')
+        if 'H' in new_point:
+            ramen_name_list.append(new_point[:new_point.index('H')])
+        else:
+            ramen_name_list.append(new_point)
+    else:
+        ramen_name_list.append(new_name)
+
+# print(ramen_name_list)
+
+for reviews in ramen_review_raw:
+    new_reviews = reviews.replace('拉麵%.G','').replace('%.G','')
+    ramen_review_list.append(new_reviews)
+    
 #### debug
 # print(len(unorganized_unorganized_shops))
 # print(unorganized_unorganized_shops)
-# print(len(ramen_shop_list))
+print(len(ramen_shop_list))
 # print(len(ramen_name_list))
 # print(len(ramen_review_list))
-# print(ramen_shop_list)
+print(ramen_shop_list)
 # print(ramen_name_list)
 # print(ramen_review_list)#cut the last 2 words and add ...
 
 # print(ramen_name_list.index("拉麵名稱價格:濃厚雞白湯拉麵雞腿捲230"))
 # print(ramen_shop_list.index('店家:樂趣Lovecheers'))
 
-# for names in ramen_name_raw:
-#     new_name = names.replace('拉麵%.G','').replace('%.G','')
-#     # print(new_name)
-#     ramen_name_raw_zero.append(new_name)
-
-for names in ramen_name_raw:
-    # print(f'original name{names}')
-    new_name = names.replace('拉麵%.G','').replace('%.G','')
-    last_ch = new_name[-1]
-    # ramen_name_M = []
-    if '0' in new_name and '00' not in new_name and '2020' not in new_name\
-        and last_ch != '）' and last_ch != ')'and '+' not in new_name:
-        ramen_name_M.append(new_name[:new_name.index('0')+1])
-    elif '00' in new_name and '2020' not in new_name:
-        ramen_name_M.append(new_name[:new_name.index('00')+2])
-    elif '0' not in new_name and '00' not in new_name and '/' not in new_name\
-        and last_ch.isdigit() == False and last_ch != '麵' and last_ch != '麺'\
-        and '+' not in new_name:
-        new_point =  new_name.replace('麵','麵M').replace('麺','麵M').replace('拉麵M名稱','拉麵名稱')
-        ramen_name_M.append(new_point)
-    else:
-        ramen_name_M.append(new_name)
-
-
-for names in ramen_name_M:
-    if 'M' in  ramen_name_M:
-        ramen_name_list.append(names[:names.index('M')])
-    else:
-        ramen_name_list.append(names)
-    
-# print(len(ramen_name_list))
-# print(ramen_name_list)       
-        
-
-
-for reviews in ramen_review_raw:
-    new_reviews = reviews.replace('拉麵%.G','').replace('%.G','')
-    ramen_review_list.append(new_reviews)
+# print(ramen_shop_list.index('店家:不二家拉麵居酒屋'))
+# print(ramen_name_list.index("拉麵名稱:醬油叉燒拉麵180"))
 
 
 
 ####csv
 # https://stackoverflow.com/questions/17704244/writing-python-lists-to-columns-in-csv
-df = pd.DataFrame(list(zip(*[ramen_shop_list, ramen_name_list, ramen_review_list])))
-col_names = ['stores', 'ramens', 'reviews']
-df.columns = col_names
-df.to_csv('fb_crawling.csv', index=True)
+# df = pd.DataFrame(list(zip(*[ramen_shop_list, ramen_name_list, ramen_review_list])))
+# col_names = ['stores', 'ramens', 'reviews']
+# df.columns = col_names
+# df.to_csv('fb_crawling.csv', index=True)
