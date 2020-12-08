911488043bfa7141e7c18e0a27ffa1b7f2b37c74
May <ym7vol@gmail.com>
2020-12-09 00:53:35 +0800

    縣市清單完成(去掉位置按鈕+圖片修改)

--- app.py
+++ app.py
@@ -38,11 +38,132 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
+    #print(type(msg))
     msg = msg.encode('utf-8')  
 
+    if event.message.text == "拉麵推薦":
+        flex_message = FlexSendMessage(
+        alt_text='hello',
+        contents={
+  "type": "bubble",
+  "hero": {
+    "type": "image",
+    "url": "https://media.timeout.com/images/105665255/630/472/image.jpg",
+    "size": "full",
+    "aspectRatio": "20:13",
+    "aspectMode": "cover",
+    "action": {
+      "type": "uri",
+      "uri": "http://linecorp.com/"
+    },
+    "position": "relative"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "你在哪裡？",
+        "weight": "bold",
+        "size": "xl",
+        "contents": []
+      },
+      {
+        "type": "text",
+        "text": "請選擇你在台灣的哪裡 ",
+        "size": "xs",
+        "color": "#888888"
+      },
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "北部",
+              "text": "北部"
+            },
+            "style": "secondary",
+            "height": "sm"
+          },
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "中部",
+              "text": "中部"
+            },
+            "style": "secondary",
+            "margin": "xxl",
+            "height": "sm"
+          }
+        ],
+        "margin": "md"
+      },
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "南部",
+              "text": "南部"
+            },
+            "style": "secondary",
+            "height": "sm"
+          },
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "東部",
+              "text": "東部"
+            },
+            "style": "secondary",
+            "margin": "xxl",
+            "height": "sm"
+          }
+        ],
+        "margin": "md"
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "button",
+        "action": {
+          "type": "message",
+          "label": "我要開定位",
+          "text": "我要開定位"
+        },
+        "style": "secondary",
+        "height": "sm",
+        "margin": "none",
+        "position": "relative",
+        "color": "#D9AE94"
+      }
+    ],
+    "spacing": "sm",
+    "margin": "none"
+  }
+}
+        )
 
-    if event.message.text == "錯誤回報":
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "請點選以下連結：https://reurl.cc/14RmVW")
+        line_bot_api.reply_message(event.reply_token,flex_message)
+
+
+
+    elif event.message.text == "錯誤回報":
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "請點選以下連結：https://reurl.cc/14RmVW"))
+
         
         
     elif event.message.text == "北部":
@@ -52,7 +173,7 @@ def handle_message(event):
   "type": "bubble",
   "hero": {
     "type": "image",
-    "url": "https://github.com/yumei86/iRamen_linebot/blob/master/image/northtw.png?raw=true",
+    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
     "size": "full",
     "aspectRatio": "20:13",
     "aspectMode": "cover",
@@ -203,7 +324,7 @@ def handle_message(event):
   "type": "bubble",
   "hero": {
     "type": "image",
-    "url": "https://github.com/yumei86/iRamen_linebot/blob/master/image/centraltw.png?raw=true",
+    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
     "size": "full",
     "aspectRatio": "20:13",
     "aspectMode": "cover",
@@ -302,7 +423,7 @@ def handle_message(event):
   "type": "bubble",
   "hero": {
     "type": "image",
-    "url": "https://github.com/yumei86/iRamen_linebot/blob/master/image/southtw.png?raw=true",
+    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
     "size": "full",
     "aspectRatio": "20:13",
     "aspectMode": "cover",
@@ -423,7 +544,7 @@ def handle_message(event):
   "type": "bubble",
   "hero": {
     "type": "image",
-    "url": "https://github.com/yumei86/iRamen_linebot/blob/master/image/easttw.png?raw=true",
+    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
     "size": "full",
     "aspectRatio": "20:13",
     "aspectMode": "cover",
