37944137fc2f6b9afb07ce3101cdca4fced3ab83
May <ym7vol@gmail.com>
2020-12-10 11:06:35 +0800

    test_for_json

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ def handle_message(event):
     msg = event.message.text
     msg = msg.encode('utf-8')  
 
-    if event.message.text == "錯誤回報":
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "ooops...\uDBC0\uDC17 \n 請點選以下連結回報問題：https://reurl.cc/14RmVW"))
+    if event.message.text == "問題回報":
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "ooops...\uDBC0\uDC17 \n請點選以下連結回報問題：https://reurl.cc/14RmVW"))
 
     elif event.message.text == "最愛清單":
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "尚未有最愛清單，快去加入你喜歡的拉麵吧！\uDBC0\uDC5e"))
