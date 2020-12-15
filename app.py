4bf514ee71580fc936b9bc98850f636277231091
May <ym7vol@gmail.com>
2020-12-15 20:16:17 +0800

    region_soup

--- app.py
+++ app.py
@@ -777,7 +777,7 @@ def handle_message(event):
 
             )
 
-            line_bot_api.reply_message(event.reply_token,flex_message4)
+                line_bot_api.reply_message(event.reply_token,flex_message4)
 
 
 
