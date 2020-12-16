ea3c7f960973d4b42bf3f02fa0567873207ebfdd
May <ym7vol@gmail.com>
2020-12-16 21:41:48 +0800

    測試＿地址

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ from linebot.models import *
 import json
 import os
 
-#----------------互叫我們的line bot(這邊直接取用herohu的環境變數)-----------------
+from flask_sqlalchemy import SQLAlchemy
+import secrets
+import csv
+
+#----------------呼叫我們的line bot(這邊直接取用herohu的環境變數)-----------------
 
 app = Flask(__name__)
 
@@ -14,6 +18,122 @@ PASS = os.environ.get('CHANNEL_SECRET')
 
 line_bot_api = LineBotApi(USER)
 handler = WebhookHandler(PASS)
+
+#----------------資料庫設定-----------------
+
+ENV = 'prod'
+
+if ENV == 'dev':
+  from dotenv import load_dotenv
+  load_dotenv()
+  SQLALCHEMY_DATABASE_URI_PRIVATE = os.getenv("SQLALCHEMY_DATABASE_URI_PRIVATE")
+  app.debug = True
+  app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI_PRIVATE
+else:
+  DATABASE_URL = os.environ.get('DATABASE_URL')
+  app.debug = False
+  app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+# # #https://flask-sqlalchemy.palletsprojects.com/en/2.x/models/
+# #---------------------------------initialize tables--------------------------
+class Main_store(db.Model):
+  __tablename__ = 'main_store'
+  store_id = db.Column (db.Integer, primary_key = True)
+  main_store = db.Column(db.String(50), nullable=False, unique = True)
+
+  detail_store_relationship = db.relationship('Store', backref= 'main_store', lazy=True)
+  post_relationship = db.relationship('Post', backref= 'main_store', lazy=True)
+  def __init__(self, main_store):
+    self.main_store = main_store
+
+class Store(db.Model):
+  __tablename__ = 'store'
+  detail_store_id = db.Column (db.String(10), primary_key = True)
+  store_id = db.Column (db.Integer, db.ForeignKey('main_store.store_id'), nullable=False, onupdate ='CASCADE')
+  store = db.Column (db.String(50), nullable=False, unique = True)
+  still_there = db.Column (db.Boolean, nullable=False)
+  address = db.Column (db.String(200))
+  discription = db.Column (db.String(500))
+  open_time = db.Column (db.String(200))
+  latitude = db.Column (db.Numeric(10,8))
+  longtitute = db.Column (db.Numeric(10,7))
+  map_review = db.Column (db.Text())
+  region = db.Column (db.String(1))
+  province = db.Column (db.String(3))
+  soup = db.Column (db.String(200))
+  transport =  db.Column (db.String(100))
+
+  store_favorite_relationship = db.relationship('Favorite', backref= 'store', lazy=True)
+
+  def __init__(self, store_id, store, still_there, address, discription,\
+     open_time, latitude, longtitute, map_review, region, province, soup, transport):
+    self.store_id = store_id
+    self.store = store
+    self.still_there = still_there
+    self.address = address
+    self.discription = discription
+    self.open_time = open_time
+    self.latitude = latitude
+    self.longtitute = longtitute
+    self.map_review = map_review
+    self.region = region
+    self.province = province
+    self.soup = soup
+    self.transport = transport
+
+class Post(db.Model):
+  __tablename__ = 'post'
+  post_id = db.Column (db.String(10), primary_key = True)
+  store_id = db.Column (db.Integer, db.ForeignKey('main_store.store_id'), nullable=False, onupdate ='CASCADE')
+  stores = db.Column (db.String(30))
+  create_on = db.Column (db.DateTime)
+  ramen_name = db.Column (db.String(100))
+  fb_review = db.Column (db.Text())
+
+  post_favorite_relationship = db.relationship('Favorite', backref= 'post', lazy=True)
+
+  def __init__(self, store_id, stores, create_on, ramen_name, fb_review):
+    self.store_id = store_id
+    self.stores = stores
+    self.create_on = create_on
+    self.ramen_name = ramen_name
+    self.fb_review = fb_review
+
+class Favorite(db.Model):
+  __tablename__ = 'favorite'
+  favorite_id = db.Column (db.String(10), primary_key = True)
+  detail_store_id = db.Column (db.String(10), db.ForeignKey('store.detail_store_id'), nullable=False, onupdate ='CASCADE')
+  post_id = db.Column (db.String(10), db.ForeignKey('post.post_id'), nullable=False, onupdate ='CASCADE')
+  line_id = db.Column (db.String(20),nullable=False)
+
+  def __init__(self, detail_store_id, post_id, line_id):
+    self.detail_store_id = detail_store_id
+    self.post_id= post_id
+    self.line_id = line_id
+
+def query_province_soup(p, s):
+    province_soup_q = db.session.query(Main_store, Store, Post)\
+                        .outerjoin(Post, Post.store_id == Main_store.store_id)\
+                        .outerjoin(Store, Store.store_id == Main_store.store_id)\
+                        .filter(Store.province == p)\
+                        .filter(Store.soup.contains(s))\
+                        .filter(Store.still_there == True)
+    return province_soup_q
+
+def query_province_direct(p):
+    province_soup_q = db.session.query(Main_store, Store, Post)\
+                        .outerjoin(Post, Post.store_id == Main_store.store_id)\
+                        .outerjoin(Store, Store.store_id == Main_store.store_id)\
+                        .filter(Store.province == p)\
+                        .filter(Store.still_there == True)
+    return province_soup_q
+
+def convert_string_to_lst(string,c): 
+    li = list(string.split(c)) 
+    return li 
 
 
 #----------------官方設定-----------------
@@ -89,9 +209,9 @@ def handle_message(event):
 
 #----------------選擇湯頭/直接推薦介面-----------------
 
-    cityname = ["台北市","新北市","基隆市","桃園市","苗栗縣","新竹縣","新竹市","台中市","彰化縣","南投縣","雲林縣","嘉義市","台南市","高雄市","屏東縣","宜蘭縣","花蓮縣","台東縣"]
+    city_name = ["台北市","新北市","基隆市","桃園市","苗栗縣","新竹縣","新竹市","台中市","彰化縣","南投縣","雲林縣","嘉義市","台南市","高雄市","屏東縣","宜蘭縣","花蓮縣","台東縣"]
 
-    for city in cityname:
+    for city in city_name:
         if event.message.text == city:
             flex_message5 = FlexSendMessage(
                         alt_text='依據你的喜好選擇吧！',
@@ -222,10 +342,59 @@ def handle_message(event):
 #----------------直接推薦/縣市看更多類似推薦-----------------
     
 
-    for city in cityname:
+    for city in city_name:
         cond = "直接推薦:"+city
         more = "看更多推薦:"+city
-        address = "query出來的地址"
+        if user_select == cond:
+#---------------------------------query 直接推薦、湯頭推薦、看更多類似推薦--------------------------
+            if ':' in user_select:
+                select_first_param = user_select[:user_select.index(':')]
+                select_second_param = user_select[user_select.index(':')+1:]
+            # print(select_second_param)
+            if (select_first_param == '直接推薦') or (select_first_param == '看更多推薦'):
+                #query withOUT soup
+                result = query_province_direct(select_second_param)
+                # print(type(result))
+                #random here
+            
+            elif select_first_param in city_name:
+                #query with soup
+                result = query_province_soup(select_first_param, select_second_param)
+                # print(type(result))
+                #random here
+            #---------------------------------put all data in a string--------------------------
+            ouput_database_fb = ''
+            ouput_database_map = ''
+            output_before_random = ''
+            for r in result:
+                try:
+                    ouput_database_fb += f'STORE:{r[1].store},ADDRESS:{r[1].address},DISCRIPTION:{r[1].discription},TRANSPORT:{r[1].transport},\
+                        FB_R_CREATE:{r[2].create_on},FB_R_RAMEN:{r[2].ramen_name},FB_R_CONTENT:{r[2].fb_review},\
+                        CHECK_TAG:{r[1].soup},CHECK_CITY:{r[1].province}%'
+                    # print('PROVINCE:{} STORE:{} ADDRESS:{} SOUP:{} MAP:{} \
+                    #   create_on:{} ramen_name:{} FB:{} '\
+                    #   .format(r[1].province, r[1].store, r[1].address, r[1].soup,r[1].map_review,\
+                    #     r[2].create_on,r[2].ramen_name, r[2].fb_review))
+                except AttributeError as error:
+                    ouput_database_map += f'STORE:{r[1].store},ADDRESS:{r[1].address},DISCRIPTION:{r[1].discription},TRANSPORT:{r[1].transport},\
+                        MAP_REVIEW:{r[1].map_review},\
+                        CHECK_TAG:{r[1].soup},CHECK_CITY:{r[1].province}%'
+            
+                # print('STORE:{} SOUP:{} MAP:{}'.format(r[1].store, r[1].soup, r[1].map_review))
+
+            # print(ouput_database_fb)
+            # print(ouput_database_map)
+            output_before_random += ouput_database_fb
+            output_before_random += ouput_database_map
+            output_before_random_clear = output_before_random.replace(u'\xa0', u' ').replace(' ','')
+            # print(output_before_random)
+            #---------------------------------change data to a list of datas--------------------------
+            output_whole_lst = convert_string_to_lst(output_before_random_clear,'%')
+            #---------------------------------random(everytime renew can auto random)--------------------------
+            output_s = secrets.choice(output_whole_lst)
+            output_lst = convert_string_to_lst(output_s, ',')
+
+        address = output_lst[2]
         if event.message.text == cond :
 
             flex_message3 = FlexSendMessage(
