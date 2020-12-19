4d53ec059802d6c8499aba87253bb2e09ca45f09
troublecat55 <yuchaodemmi@gmail.com>
2020-12-19 17:29:01 +0100

    add post and get request

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-ENV = 'prod'
+ENV = 'dev'
 
 if ENV == 'dev':
   from dotenv import load_dotenv
@@ -78,8 +78,6 @@ class Post(db.Model):
   ramen_name = db.Column (db.String(100))
   fb_review = db.Column (db.Text())
 
-  post_favorite_relationship = db.relationship('Favorite', backref= 'post', lazy=True)
-
   def __init__(self, store_id, stores, create_on, ramen_name, fb_review):
     self.store_id = store_id
     self.stores = stores
@@ -89,18 +87,14 @@ class Post(db.Model):
 
 class Favorite(db.Model):
   __tablename__ = 'favorite'
-  favorite_id = db.Column (db.String(10), primary_key = True)
-  detail_store_id = db.Column (db.String(10), db.ForeignKey('store.detail_store_id'), nullable=False, onupdate ='CASCADE')
-  post_id = db.Column (db.String(10), db.ForeignKey('post.post_id'), nullable=False, onupdate ='CASCADE')
-  line_id = db.Column (db.String(20),nullable=False)
-
-  def __init__(self, detail_store_id, post_id, line_id):
-    self.detail_store_id = detail_store_id
-    self.post_id= post_id
+  id = db.Column (db.Integer, primary_key = True)
+  line_id = db.Column (db.String(20), nullable = False)
+  detail_store_id = db.Column (db.String(10), db.ForeignKey('store.detail_store_id'), nullable = False, onupdate ='CASCADE')
+  
+  def __init__(self, line_id, detail_store_id):
     self.line_id = line_id
-
-
-
+    self.detail_store_id = detail_store_id
+    
 
 
 
@@ -109,13 +103,10 @@ class Favorite(db.Model):
 # db.create_all() #https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/
 # exit()
 
-## query
-# .filter(Store.soup.contains('二郎風'))
-# .add_columns(Store.soup, Store.province, Store.store, Store.address,\
-#                           Store.discription, Store.open_time, Store.transport,\
-#                           Post.create_on, Post.ramen_name, Post.fb_review)\
+# query
 # print(province_soup_q) #(<Main_store 0>, <Store 2223>, <Post 3331>)
 
+   
 # query
 def query_province_soup(p, s):
   province_soup_q = db.session.query(Main_store, Store, Post)\
@@ -155,25 +146,29 @@ def convert_string_to_lst(string,c):
 
 city_name = ["台北市","新北市","基隆市","桃園市","苗栗縣","新竹縣","新竹市"\
             ,"台中市","彰化縣","南投縣","雲林縣","嘉義市","台南市","高雄市","屏東縣","宜蘭縣","花蓮縣","台東縣"]
-user_select = '中山'
 
+user_msg = 'query出來的地址'
+user_select = '直接推薦:雲林縣'
+select_first_param = ''
+select_second_param = ''
 
-#---------------------------------query 直接推薦、湯頭推薦、看更多類似推薦--------------------------
 if ':' in user_select:
   select_first_param = user_select[:user_select.index(':')]
   select_second_param = user_select[user_select.index(':')+1:]
-  # print(select_second_param)
-  if (select_first_param == '直接推薦') or (select_first_param == '看更多推薦'):
-    #query withOUT soup
-    result = query_province_direct(select_second_param)
-    # print(type(result))
-    #random here
-  
-  elif select_first_param in city_name:
-    #query with soup
-    result = query_province_soup(select_first_param, select_second_param)
-    # print(type(result))
-    #random here
+# print(select_second_param)
+
+if select_first_param != '' and select_first_param != ''and\
+  (select_first_param == '直接推薦') or (select_first_param == '看更多推薦'):
+  #query withOUT soup
+  result = query_province_direct(select_second_param)
+  # print(type(result))
+  #random here
+
+elif select_first_param in city_name:
+  #query with soup
+  result = query_province_soup(select_first_param, select_second_param)
+  # print(type(result))
+  #random here
 #+++++++++++++++++CHANGE+++++++++++++++++
 elif ' ' in user_select and ' ' not in user_select[-1] and ' ' not in user_select[0]:
   input_store = ''
@@ -190,6 +185,7 @@ else:
 #+++++++++++++++++CHANGE+++++++++++++++++
 
 
+#MAP_ID:{r[1].detail_store_id},FB_ID:{r[2].post_id}
 #---------------------------------put all data in a string--------------------------
 ouput_database_fb = ''
 ouput_database_map = ''
@@ -198,7 +194,8 @@ for r in result:
   try:
       ouput_database_fb += f'STORE:{r[1].store},ADDRESS:{r[1].address},DISCRIPTION:{r[1].discription},TRANSPORT:{r[1].transport},\
           FB_R_CREATE:{r[2].create_on},FB_R_RAMEN:{r[2].ramen_name},FB_R_CONTENT:{r[2].fb_review},\
-          CHECK_TAG:{r[1].soup},CHECK_CITY:{r[1].province}%'
+          CHECK_TAG:{r[1].soup},CHECK_CITY:{r[1].province},\
+          MAP_ID:{r[1].detail_store_id}%'
   # print('PROVINCE:{} STORE:{} ADDRESS:{} SOUP:{} MAP:{} \
   #   create_on:{} ramen_name:{} FB:{} '\
   #   .format(r[1].province, r[1].store, r[1].address, r[1].soup,r[1].map_review,\
@@ -206,8 +203,8 @@ for r in result:
   except AttributeError as error:
       ouput_database_map += f'STORE:{r[1].store},ADDRESS:{r[1].address},DISCRIPTION:{r[1].discription},TRANSPORT:{r[1].transport},\
         MAP_REVIEW:{r[1].map_review},\
-        CHECK_TAG:{r[1].soup},CHECK_CITY:{r[1].province}%'
-
+        CHECK_TAG:{r[1].soup},CHECK_CITY:{r[1].province},\
+        MAP_ID:{r[1].detail_store_id}%'
 
 
 # print(ouput_database_fb)
@@ -227,10 +224,93 @@ for data in output_whole_lst:
 try:
   output_s = secrets.choice(output_whole_lst)
   output_lst = convert_string_to_lst(output_s, ',')
-  print(output_lst)
+  # print(output_lst)
 except IndexError as error:
   print("請輸入有效店名關鍵字(不可在前後加入空白)，例如\"鷹流 中山\",\"一風堂\"")
 #+++++++++++++++++CHANGE+++++++++++++++++
+
+##---------------------------------------favorite list-------------------------------------
+user_line_id = 'demmifish'
+msg = '加到最愛清單:一本拉麵·梟'
+first_love_param = ''
+second_love_param = ''
+if ':' in msg:
+  first_love_param = msg[:msg.index(':')]
+  second_love_param = msg[msg.index(':')+1:]
+# print( first_love_param )
+# print( second_love_param )
+def get_store_id(store_name):
+  store_id_q = db.session.query(Store)\
+      .filter(Store.store == store_name)
+  for data in store_id_q:
+    get_id = data.detail_store_id
+  return get_id
+def store_exist(store_name):
+  store_exist = db.session.query(Store, Favorite)\
+        .join(Favorite, Favorite.detail_store_id == Store.detail_store_id)\
+        .filter(Store.store == store_name).count()
+  return store_exist
+def count_love_list(user_id):
+  count_love_list = db.session.query(Favorite)\
+        .filter(Favorite.line_id == user_id).count()
+def count_total_row(database_name):
+  count_total_row = db.session.query(database_name).count()
+  return count_total_row
+
+
+#---------------------------------測試用---------------------------
+@app.route('/')
+def home():
+  return 'hey'
+@app.route('/submit', methods=['POST'])
+def submit():
+#---------------------------------測試用---------------------------
+  if request.method =='POST':
+    if first_love_param == '加到最愛清單':
+      favorite_list_count = count_love_list(user_line_id)
+      already_add_store_count = store_exist(second_love_param)
+      get_foreign_id = get_store_id(second_love_param)
+      # print(type(user_line_id))
+      # print(type(get_foreign_id))
+      if favorite_list_count == 0 or\
+         (favorite_list_count != 0 and already_add_store_count == 0):
+          # print(get_foreign_id)
+        data = Favorite(user_line_id, get_foreign_id)
+        db.session.add(data)
+        db.session.commit()
+        return'成功加到最愛清單耶耶耶'
+      elif favorite_list_count > 25:
+        return'最愛清單太長了要刪'
+      else:
+        return'已經加過這間店到最愛清單'
+
+
+##---------------------------------------Query love-list by userID-------------------------------------
+# # l[0]:Store l[1]:favorite
+def get_list_from_user_id(user_id):  
+  love_list_q = db.session.query(Store,Favorite)\
+    .outerjoin(Favorite, Favorite.detail_store_id == Store.detail_store_id)\
+    .filter(Favorite.line_id == user_id)
+  return love_list_q
+
+love_lst_q = get_list_from_user_id(user_line_id)
+love_list = ''
+for l in love_lst_q:
+  love_list += f'STORE:{l[0].store},ADDRESS:{l[0].address},DISCRIPTION:{l[0].discription},TRANSPORT:{l[0].transport},MAP_REVIEW:{l[0].map_review},CITY:{l[0].province},CHECK_TAG:{l[0].soup}%'
+output_whole_love_list = convert_string_to_lst(love_list,'%')
+for data in output_whole_love_list:
+  if data == '':
+    output_whole_love_list.remove(data)
+
+print(output_whole_love_list)
+
+# #count how many list items
+# item_count_love_list = db.session.query(Favorite).filter(Favorite.line_id == user_line_id).count()
+# print(item_count_love_list)
+
+# if __name__ == 'main':
+app.run(debug=True, port=8000)
+
 
 
 
@@ -283,5 +363,4 @@ except IndexError as error:
 #     for key in soup_data.keys():
 #         f.write("%s,%s\n"%(key,soup_data[key]))
 
-if __name__ == 'main':
-  app.run(debug=True)
+
