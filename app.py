62f50a6e7450258a87887f7f8b85b3f3c4639c1f
May <ym7vol@gmail.com>
2020-12-21 16:54:41 +0800

    測試_最愛清單template連接

--- app.py
+++ app.py
@@ -1403,11 +1403,13 @@ def handle_message(event):
                 try:
                     ouput_database_fb += f'STORE:{r[1].store},ADDRESS:{r[1].address},DISCRIPTION:{r[1].discription},TRANSPORT:{r[1].transport},\
                         FB_R_CREATE:{r[2].create_on},FB_R_RAMEN:{r[2].ramen_name},FB_R_CONTENT:{r[2].fb_review},\
+                        LONGITUDE:{r[1].longtitute},LATITUDE:{r[1].latitude},\
                         CHECK_TAG:{r[1].soup},CHECK_CITY:{r[1].province}%'
 
                 except AttributeError:
                     ouput_database_map += f'STORE:{r[1].store},ADDRESS:{r[1].address},DISCRIPTION:{r[1].discription},TRANSPORT:{r[1].transport},\
                         MAP_REVIEW:{r[1].map_review},\
+                        LONGITUDE:{r[1].longtitute},LATITUDE:{r[1].latitude},\
                         CHECK_TAG:{r[1].soup},CHECK_CITY:{r[1].province}%'
                     
 
@@ -1429,15 +1431,20 @@ def handle_message(event):
             descrip = output_lst[2][output_lst[2].index(':')+1:]
             trans = output_lst[3][output_lst[3].index(':')+1:]
 
-            if len(output_lst) == 9:
+            if len(output_lst) == 11:
                 #FB評論
                 c1 = output_lst[4][output_lst[4].index(':')+1:]
                 c2 = output_lst[5][output_lst[5].index(':')+1:]
                 c3 = output_lst[6][output_lst[6].index(':')+1:]
                 comment = f'貼文時間：\n{c1}\n品項：\n{c2}\n評論：\n{c3}'
-            elif len(output_lst) == 7:
+                lon = output_lst[7][output_lst[7].index(':')+1:]
+                lat = output_lst[8][output_lst[8].index(':')+1:]
+
+            elif len(output_lst) == 9:
                 #googleMap
                 comment = output_lst[4][output_lst[4].index(':')+1:]
+                lon = output_lst[5][output_lst[5].index(':')+1:]
+                lat = output_lst[6][output_lst[6].index(':')+1:]  
 
             if event.message.text == n:
 
