cb1f9f682b622393ddcf2f410f25415c125819a2
troublecat55 <yuchaodemmi@gmail.com>
2021-01-03 19:48:39 +0100

    debug again

--- app.py
+++ app.py
@@ -1989,20 +1989,14 @@ def handle_message(event):
                 output_whole_lst.remove(data)
         #---------------------------------random(everytime renew can auto random)--------------------------
         if len(output_whole_lst) != 0:
-            if len(output_whole_lst) > 1:
-                try:
-                    output_s = secrets.choice(output_whole_lst)
-                    output_lst = convert_string_to_lst(output_s, ',')
-
-                except IndexError:
-                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "\udbc0\udcb2打字搜尋功能請輸入:\n關鍵字 關鍵字,\n例如\n\"鷹流 中山\",\"七 面鳥\",\"麵屋秋 匠\"\
-                                                                                          \n\udbc0\udcb2請輸入有效店名關鍵字(中間幫我留空,但不可在前後加入空白)\
-                                                                                          \n\udbc0\udcb2或請幫我直接點選拉麵推薦選單做選擇喔！")
-                    )
-    
-            elif len(output_whole_lst) == 1:
-                output_s = output_whole_lst
+            try:
+                output_s = secrets.choice(output_whole_lst)
                 output_lst = convert_string_to_lst(output_s, ',')
+            except IndexError:
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "\udbc0\udcb2打字搜尋功能請輸入:\n關鍵字 關鍵字,\n例如\n\"鷹流 中山\",\"七 面鳥\",\"麵屋秋 匠\"\
+                                                                                      \n\udbc0\udcb2請輸入有效店名關鍵字(中間幫我留空,但不可在前後加入空白)\
+                                                                                      \n\udbc0\udcb2或請幫我直接點選拉麵推薦選單做選擇喔！")
+                )
 
         else:
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "\udbc0\udcb2打字搜尋功能請輸入:\n關鍵字 關鍵字,\n例如\n\"鷹流 中山\",\"七 面鳥\",\"麵屋秋 匠\"\
