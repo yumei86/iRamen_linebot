2416a1521e7d6de424ea01db08ad7ed7b6dd0839
troublecat55 <yuchaodemmi@gmail.com>
2021-02-09 18:55:46 +0100

    change join method

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 import secrets
 import random
-from functools import reduce
 import csv
 
 #----------------呼叫我們的line bot(這邊直接取用heroku的環境變數)-----------------
@@ -1961,7 +1960,8 @@ def handle_message(event):
         store_example = ['「鷹流 公館」','「公 子」','「山下公 園」','「隱家 赤峰」','「七 面鳥」','「麵屋 壹」','「真 劍」','「麵 屋秋匠」','「Mr 拉麵雲」','「辰 拉」','「京都 柚子」','「麵屋 ichi」']
         random.shuffle(store_example)
         store_example_choice_lst = store_example[:5]
-        store_example_choice = reduce(lambda a,x: a+str(x), store_example_choice_lst, "")
+        store_example_choice = ''.join(store_example_choice_lst)
+        # store_example_choice = reduce(lambda a,x: a+str(x), store_example_choice_lst, "")
 
         if ' ' in user_select and ' ' not in user_select[-1] and ' ' not in user_select[0]:
             input_lst = user_select.split()
