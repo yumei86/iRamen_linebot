ca738569391c50afec32d74f87365e13a4de95e6
troublecat55 <yuchaodemmi@gmail.com>
2021-03-01 22:39:57 +0100

    debug and format

--- app.py
+++ app.py
@@ -894,34 +894,35 @@ def handle_message(event):
             #---------------------------------change data to a list of datas--------------------------
             output_whole_lst = convert_string_to_lst(output_before_random_clear,'%')
             output_whole_lst = [i for i in output_whole_lst if i]
-        #---------------------------------random(everytime renew can auto random)--------------------------
-        output_s = random.choice(output_whole_lst)
-        output_lst = convert_string_to_lst(output_s, ',')
-        if len(output_lst) == 12 or len(output_lst) == 10:
-            store_n = output_lst[0][output_lst[0].index(':')+1:]
-            address = output_lst[1][output_lst[1].index(':')+1:]
-            descrip = output_lst[2][output_lst[2].index(':')+1:]
-            trans = output_lst[3][output_lst[3].index(':')+1:]
-            f_city = output_lst[-1][output_lst[-1].index(':')+1:]
-            if len(output_lst) == 12:
-                #FB評論
-                c1 = output_lst[4][output_lst[4].index(':')+1:]
-                c2 = output_lst[5][output_lst[5].index(':')+1:]
-                c3 = output_lst[6][output_lst[6].index(':')+1:]
-                comment = f'貼文時間：\n{c1}\n\n品項：\n{c2}\n\n評論：\n{c3}'
-                lon = output_lst[7][output_lst[7].index(':')+1:]
-                lat = output_lst[8][output_lst[8].index(':')+1:]
-                op  = output_lst[9][output_lst[9].index(':')+1:]
+            #---------------------------------random(everytime renew can auto random)--------------------------
+            output_s = random.choice(output_whole_lst)
+            output_lst = convert_string_to_lst(output_s, ',')
+            if len(output_lst) == 12 or len(output_lst) == 10:
+                store_n = output_lst[0][output_lst[0].index(':')+1:]
+                address = output_lst[1][output_lst[1].index(':')+1:]
+                descrip = output_lst[2][output_lst[2].index(':')+1:]
+                trans = output_lst[3][output_lst[3].index(':')+1:]
+                f_city = output_lst[-1][output_lst[-1].index(':')+1:]
+                if len(output_lst) == 12:
+                    #FB評論
+                    c1 = output_lst[4][output_lst[4].index(':')+1:]
+                    c2 = output_lst[5][output_lst[5].index(':')+1:]
+                    c3 = output_lst[6][output_lst[6].index(':')+1:]
+                    comment = f'貼文時間：\n{c1}\n\n品項：\n{c2}\n\n評論：\n{c3}'
+                    lon = output_lst[7][output_lst[7].index(':')+1:]
+                    lat = output_lst[8][output_lst[8].index(':')+1:]
+                    op  = output_lst[9][output_lst[9].index(':')+1:]
 
-            elif len(output_lst) == 10:
-                #googleMap
-                comment = output_lst[4][output_lst[4].index(':')+1:]
-                lon = output_lst[5][output_lst[5].index(':')+1:]
-                lat = output_lst[6][output_lst[6].index(':')+1:] 
-                op  = output_lst[7][output_lst[7].index(':')+1:]  
-            else:
-                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "\udbc0\udcb2出錯啦靠邀，麻煩您把「錯誤代碼O1」和「您的店家搜尋指令（含空格）」填在填錯誤回報上，感激到五體投地\udbc0\udcb2") )
-            flex_message3 = FlexSendMessage(
+                elif len(output_lst) == 10:
+                    #googleMap
+                    comment = output_lst[4][output_lst[4].index(':')+1:]
+                    lon = output_lst[5][output_lst[5].index(':')+1:]
+                    lat = output_lst[6][output_lst[6].index(':')+1:] 
+                    op  = output_lst[7][output_lst[7].index(':')+1:]  
+                else:
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "\udbc0\udcb2出錯啦靠邀，麻煩您把「錯誤代碼O1」和「您的店家搜尋指令（含空格）」填在填錯誤回報上，感激到五體投地\udbc0\udcb2") )
+            
+                flex_message9 = FlexSendMessage(
                                             alt_text='快回來看看我幫你找到的店家！',
                                             contents= {
                                                             "type": "carousel",
@@ -1222,10 +1223,10 @@ def handle_message(event):
                                                             ]
                                                             }
                                 )
-                        line_bot_api.reply_message(event.reply_token,flex_message3)
+                line_bot_api.reply_message(event.reply_token,flex_message9)
 
-        else:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text = f"資料庫有誤"))
+            else:
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = f"資料庫有誤"))
             
     elif ' ' in event.message.text and ' ' not in event.message.text[-1] and ' ' not in event.message.text[0]:
         user_select = event.message.text
