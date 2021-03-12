280f5dae23c293bb657d9088a16a029c398f3519
troublecat55 <yuchaodemmi@gmail.com>
2021-03-12 12:15:05 +0100

    try to add qr in foreign country reply distance

--- app.py
+++ app.py
@@ -1865,7 +1865,10 @@ def handle_location(event):
     # 算距離
     # '''
     if all_store_province == '':
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text= "\udbc0\udcb2目前不支援離島與國外拉麵店，請到台灣本島吃拉麵~" ))
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text= "\udbc0\udc7B目前不支援離島與國外拉麵店，請到台灣本島吃拉麵~" ),
+                                                                     quick_reply=QuickReply(items=[
+                                                                        QuickReplyButton(action=LocationAction(label="再定位一次My LOC"))
+                                                                    ]))
     else:
         sorted_city_distance_dic = caculate_distance(user_location,all_store_province)
         if  len(sorted_city_distance_dic) >= 10:
