1317b83543407a84db4ee8cdc9f680d1851b9ca9
troublecat55 <yuchaodemmi@gmail.com>
2021-03-22 14:04:01 +0100

    really the last itme fixing template

--- msg_template/Flex_template.py
+++ msg_template/Flex_template.py
@@ -234,8 +234,11 @@ def soup_direct_flex(whole_input):
                                               },
                                               "style": "primary",
                                               "margin": "md",
-                                              "height": "sm",
                                               "color": "#797D62"
+                                            },
+                                            {
+                                            "type": "spacer",
+                                            "size": "sm"
                                             }
                                           ]
                                         }
